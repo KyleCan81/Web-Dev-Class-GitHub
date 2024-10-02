68b9cc45e5b933e0afe338e4159548c59e54ba0c
KyleCan81 <Kylecan8112@gmail.com>
2024-10-02 10:24:46 -0400

    comment

---
+++ 10-2 classwork/index.js
@@ -0,0 +1,28 @@
+//Model for dog
+
+//Properties
+//Name
+//Height
+//Health
+//Hunger
+//Thirst
+//Color
+//Breed
+//Has collar
+//Has leash
+//Has toy
+
+//Events
+//eat food
+//drink water
+//walk forward/left/right/down
+//jump
+//sit down
+//lay down
+//wag tail
+//wear collar
+//wear leash
+//grab toy
+
+//Methods
+//
